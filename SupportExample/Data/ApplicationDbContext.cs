c5989b55ae19ad40ba45308f7418879ac629e9e8
limematheuz <mribeiro17.info@gmail.com>
2024-06-20 12:06:26 +0200

    create projectModel and MigratrionsProject

---
+++ SupportExample/Data/ApplicationDbContext.cs
@@ -0,0 +1,9 @@
+﻿using Microsoft.EntityFrameworkCore;
+using SupportExample.Models;
+
+namespace SupportExample.Data;
+
+public class ApplicationDbContext(DbContextOptions options) : DbContext(options)
+{
+    public DbSet<Project> Projects { get; set; }
+}
--- SupportExample/Program.cs
+++ SupportExample/Program.cs
@@ -1,9 +1,17 @@
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Options;
+using SupportExample.Data;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+builder.Services.AddDbContext<ApplicationDbContext>(options =>
+{
+    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection"));
+});
 
 var app = builder.Build();
 
